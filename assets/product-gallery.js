0e2f42655f5a4f67feb79a6dbf3dd5b184df482c
marloweR02 <marlowe.ramirez02@gmail.com>
2026-05-18 15:31:53 +0800

    save

---
+++ assets/product-gallery.js
@@ -0,0 +1,46 @@
+class ProductGallery extends HTMLElement{
+  constructor(){
+    super()
+
+    this.navLeft = this.querySelector('[nav-left]')
+    this.navRight = this.querySelector('[nav-right]')
+    this.featuredImage = this.querySelectorAll('.product-gallery__image')
+    this.featureCarousel = this.querySelector('.product-gallery__carousel')
+
+    this.currentIndex = 0
+  }
+
+  connectedCallback(){
+    this.navLeft.addEventListener('click', this.slideLeft.bind(this))
+    this.navRight.addEventListener('click', this.slideRight.bind(this))
+  }
+
+  slideLeft(){
+    this.currentIndex--
+
+    if( this.currentIndex < 0){
+      this.currentIndex = this.featuredImage.length - 1
+    }
+
+    this.updateFeaturedImage()
+
+  }
+  slideRight(){
+    this.currentIndex++
+
+    if( this.currentIndex >= this.featuredImage.length){
+      this.currentIndex = 0
+    }
+
+    this.updateFeaturedImage()
+  }
+
+  updateFeaturedImage(){
+    const offset = this.currentIndex * 100
+    this.featureCarousel.style.transform = `translateX(-${offset}%)`
+  }
+
+  disconnectedCallback(){}
+}
+
+customElements.define("product-gallery", ProductGallery)
