90234a46dafddf132b8ba22b82061eceddd7f878
marloweR02 <marlowe.ramirez02@gmail.com>
2026-05-20 11:02:04 +0800

    save

--- assets/animation.js
+++ assets/animation.js
@@ -4,7 +4,7 @@ const observerFadeInUp = new IntersectionObserver((entries, observer)=>{
   entries.forEach((entry, index)=>{
     if(entry.isIntersecting){
       entry.target.classList.add('animate')
-      entry.target.style.animationDelay = `${index * 200}ms`
+      entry.target.style.animationDelay = `${index * 220}ms`
       observer.unobserve(entry.target)
     }
   })
